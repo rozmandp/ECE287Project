cec91d073b6841211e337df311c3330bd738cca8
idn0971 <57015466+idn0971@users.noreply.github.com>
2019-11-04 18:01:27 -0500

    Add files via upload

--- 287Project.vhd
+++ 287Project.vhd
@@ -32,6 +32,30 @@ begin
   Y <= output;
 end behavioral;
 
+ENTITY register32 IS PORT(
+    d   : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+    ld  : IN STD_LOGIC; -- load/enable.
+    clr : IN STD_LOGIC; -- async. clear.
+    clk : IN STD_LOGIC; -- clock.
+    q   : OUT STD_LOGIC_VECTOR(7 DOWNTO 0) -- output
+);
+END register32;
+
+ARCHITECTURE description OF register8 IS
+
+BEGIN
+    process(clk, clr)
+    begin
+        if clr = '1' then
+            q <= "00000000";
+        elsif rising_edge(clk) then
+            if ld = '1' then
+                q <= d;
+            end if;
+        end if;
+    end process;
+END description;
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
